314b7846602b0de78e387305d4d00e09b92cc3b7
Danzel Rana <danzelrana@gmail.com>
2020-01-08 13:05:25 -0800

    signin routing

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,6 +1,39 @@
 import React from "react";
 
 class Signin extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      signInEmail: "",
+      signInPassword: ""
+    };
+  }
+
+  onEmailChange = event => {
+    this.setState({ signInEmail: event.target.value });
+  };
+
+  onPasswordChange = event => {
+    this.setState({ signInPassword: event.target.value });
+  };
+
+  onSubmitSignIn = async () => {
+    try {
+      const res = await fetch("http://localhost:3000/signin", {
+        method: "post",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          email: this.state.signInEmail,
+          password: this.state.signInPassword
+        })
+      });
+      const json = await res.json();
+      if (json === "success") this.props.onRouteChange("home");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   render() {
     const { onRouteChange } = this.props;
     return (
@@ -18,6 +51,7 @@ class Signin extends React.Component {
                   type="email"
                   name="email-address"
                   id="email-address"
+                  onChange={this.onEmailChange}
                 />
               </div>
               <div className="mv3">
@@ -29,6 +63,7 @@ class Signin extends React.Component {
                   type="password"
                   name="password"
                   id="password"
+                  onChange={this.onPasswordChange}
                 />
               </div>
             </fieldset>
@@ -37,7 +72,7 @@ class Signin extends React.Component {
                 className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
                 type="submit"
                 value="Sign in"
-                onClick={() => onRouteChange("home")}
+                onClick={this.onSubmitSignIn}
               />
             </div>
             <div className="lh-copy mt3">
