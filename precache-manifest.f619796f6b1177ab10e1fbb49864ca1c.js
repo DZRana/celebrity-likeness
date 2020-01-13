2c24ef4606fbdce36f36717d1906cf2a63f3aa01
Danzel Rana <danzelrana@gmail.com>
2020-01-13 12:06:23 -0800

    Updates

---
+++ precache-manifest.f619796f6b1177ab10e1fbb49864ca1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d476fca9b8334fc2fd907de3cdb17c22",
+    "url": "/celebrity-likeness/index.html"
+  },
+  {
+    "revision": "5cc8d6141279d9b2532c",
+    "url": "/celebrity-likeness/static/css/2.632827f6.chunk.css"
+  },
+  {
+    "revision": "8a3ee41be531ed71e774",
+    "url": "/celebrity-likeness/static/css/main.ee838573.chunk.css"
+  },
+  {
+    "revision": "5cc8d6141279d9b2532c",
+    "url": "/celebrity-likeness/static/js/2.b4a06623.chunk.js"
+  },
+  {
+    "revision": "e6001fb061ff520794147ccccd3e116e",
+    "url": "/celebrity-likeness/static/js/2.b4a06623.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8a3ee41be531ed71e774",
+    "url": "/celebrity-likeness/static/js/main.7685fdd6.chunk.js"
+  },
+  {
+    "revision": "23743ebd7c84c7260605",
+    "url": "/celebrity-likeness/static/js/runtime-main.fed26275.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/celebrity-likeness/precache-manifest.f619796f6b1177ab10e1fbb49864ca1c.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/celebrity-likeness/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
